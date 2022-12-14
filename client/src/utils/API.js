2012dc7395c354a7a3559d07630810fafcce89cc
Gnubgnub <108098402+Gnubgnub@users.noreply.github.com>
2022-12-14 17:46:30 -0600

    Update API.js
    Signed-off-by: Gnubgnub <108098402+Gnubgnub@users.noreply.github.com>

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,83 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css';
+// route to get logged in user's info (needs the token)
+export const getMe = (token) => {
+    return fetch('/api/users/me', {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+    });
+  };
 
-// Uncomment import statement below after building queries and mutations
-// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+  export const createUser = (userData) => {
+    return fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
 
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import Stats from './pages/Stats';
-import Navbar from './components/Navbar'
-import LoginForm from './components/LoginForm';
-import SignupForm from './components/SingupForm'
-
-function App() {
-  return (
-    <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-      <Navbar />
-        <Routes>
-          <Route 
-            path="/" 
-            element={<Home />} 
-          />
-          <Route
-          path="/signup"
-          element={<SignupForm />}
-          />
-           <Route
-          path="/login"
-          element={<LoginForm />}
-          />
-          {/* <Route 
-            path="/SignIn" 
-            element={<SignIn />} 
-          />
-          <Route 
-            path="/Stats/:id" 
-            element={<Stats />} 
-          /> */}
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-/* import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
-function NavbarDarkExample() {
-  return (
-    <Navbar variant="dark" bg="dark" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-dark-example" />
-        <Navbar.Collapse id="navbar-dark-example">
-          <Nav>
-            <NavDropdown
-              id="nav-dropdown-dark-example"
-              title="Dropdown"
-              menuVariant="dark"
-            >
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
-
-export default NavbarDarkExample; */
+  export const loginUser = (userData) => {
+    return fetch('/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
