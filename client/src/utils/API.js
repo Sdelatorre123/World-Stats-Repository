fe851ecc0e2074ef633bdae2f4649f0130a3499c
Cronus <GuleedAli@proton.me>
2022-12-14 19:27:08 -0600

    changes to my API.js folder

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -27,13 +27,3 @@ export const getMe = (token) => {
       body: JSON.stringify(userData),
     });
   };
-
-  export const loginUser = (userData) => {
-    return fetch('/api/users/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
-  };
